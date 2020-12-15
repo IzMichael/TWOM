4aeddaf391c535ba5bb8f0fcbe6bac3bdbaf1b40
IzMichael <themichael77@outlook.com>
2020-12-15 18:19:15 +1300

    Develop Version 1

---
+++ assets/js/import-html.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('header').load("/universal-assets/header.html");
+  });
+
+$(document).ready(function(){
+    $('footer').load("/universal-assets/footer.html");
+  });
+
+$(document).ready(function(){
+    $('menu').load("/universal-assets/menu.html");
+  });
